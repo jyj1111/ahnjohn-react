8c64a56537cd18b12d9dc7c05cab96470e15c896
jyj <jyj202040@gmail.com>
2022-07-22 21:27:14 +0900

    ahnjohn-react create

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
